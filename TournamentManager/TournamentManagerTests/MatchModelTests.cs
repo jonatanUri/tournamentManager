3497fdf308090c6c7c8a8a40b723500cc5250eeb
Jonatán Uri <urijoni@gmail.com>
2019-12-16 10:31:03 +0100

    Unit tests

---
+++ TournamentManager/TournamentManagerTests/MatchModelTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using TournamentManager.Models;
+
+namespace TournamentManagerTests
+{
+    [TestFixture]
+    public class MatchModelTests
+    {
+        TeamModel team1;
+        TeamModel team2;
+        MatchModel match;
+
+        [SetUp]
+        public void SetUp()
+        {
+            team1 = new TeamModel("Team1");
+            team2 = new TeamModel("Team2");
+            match = new MatchModel(team1, team2);
+        }
+
+        [Test]
+        public void SetResult_Given16And9_ShouldSetWinnerToTeam1()
+        {
+            match.SetResult(16, 9);
+            Assert.AreEqual(team1, match.winner);
+        }
+
+        [Test]
+        public void SetResult_Given1And2_ShouldSetWinnerToTeam2()
+        {
+            match.SetResult(1, 2);
+            Assert.AreEqual(team2, match.winner);
+        }
+
+    }
+}
---
+++ TournamentManager/TournamentManagerTests/RoundModelTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using TournamentManager.Models;
+
+namespace TournamentManagerTests
+{
+    [TestFixture]
+    class RoundModelTests
+    {
+        TeamModel team1;
+        TeamModel team2;
+        TeamModel team3;
+        TeamModel team4;
+        List<TeamModel> teams;
+        RoundModel round;
+
+        [SetUp]
+        public void SetUp()
+        {
+            team1 = new TeamModel("Team1");
+            team2 = new TeamModel("Team2");
+            team3 = new TeamModel("Team3");
+            team4 = new TeamModel("Team4");
+            teams = new List<TeamModel>();
+            teams.Add(team1);
+            teams.Add(team2);
+            teams.Add(team3);
+            teams.Add(team4);
+            round = new RoundModel(teams);
+        }
+
+        [Test]
+        public void CreateNextRound_ShouldSetNextRoundWithWinningTeams()
+        {
+            List<TeamModel> WinnerTeams = new List<TeamModel>();
+            foreach (MatchModel match in round.Matches)
+            {
+                match.winner = match.team1;
+                WinnerTeams.Add(match.team1);
+            }
+            round.CreateNextRound();
+            Assert.AreEqual(WinnerTeams, round.NextRound.Teams);
+        }
+    }
+}
