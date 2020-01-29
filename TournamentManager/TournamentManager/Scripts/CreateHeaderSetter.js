21d661e5b3179ca39d50849a46643f02cf782d55
Jonatán Uri <urijoni@gmail.com>
2020-01-29 16:34:28 +0100

    Create page fancyfied

---
+++ TournamentManager/TournamentManager/Scripts/CreateHeaderSetter.js
@@ -0,0 +1,12 @@
+﻿var NameInput = document.getElementById("Name");
+var HeaderText = document.getElementById("header-text");
+
+function SetHeaderTextToNameInput() {
+    HeaderText.innerText = NameInput.value;
+    if (NameInput.value == "") {
+        HeaderText.innerText = "New tournament";
+    }
+}
+
+NameInput.onkeydown = function () { setTimeout(SetHeaderTextToNameInput, 5) };
+
